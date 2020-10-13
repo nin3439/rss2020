5e5fd03d1a713e2fc8e14bd1da42aad8236b2a52
nin3439 <ninok33_94@mail.ru>
2020-10-13 20:20:54 +0300

    feat: finish to implement calculator

--- calculator/script.js
+++ calculator/script.js
@@ -2,9 +2,8 @@ const calculator = document.querySelector(".calculator");
 const keys = calculator.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
 
-
-keys.addEventListener('click', function (e) {
-  if (e.target.matches('button')) {
+keys.addEventListener("click", function (e) {
+  if (e.target.matches("button")) {
     const key = e.target;
     const action = key.dataset.action;
     const keyContent = key.textContent;
@@ -13,16 +12,16 @@ keys.addEventListener('click', function (e) {
 
     if (!action) {
       if (
-        displayedNum === '0' ||
-        previousKeyType === 'operator' ||
-        previousKeyType === 'calculate'
+        displayedNum === "0" ||
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
       ) {
         display.textContent = keyContent;
       } else {
         display.textContent = displayedNum + keyContent;
       }
-      calculator.dataset.previousKeyType = 'number';
-    };
+      calculator.dataset.previousKeyType = "number";
+    }
 
     if (
       action === "add" ||
@@ -36,11 +35,13 @@ keys.addEventListener('click', function (e) {
       const operator = calculator.dataset.operator;
       const secondValue = displayedNum;
 
-      if (
+      if (action === "square-root" && previousKeyType === "uno-minus") {
+        display.textContent = "Error";
+      } else if (
         firstValue &&
         operator &&
-        previousKeyType !== 'operator' &&
-        previousKeyType !== 'calculate'
+        previousKeyType !== "operator" &&
+        previousKeyType !== "calculate"
       ) {
         const calcValue = calculate(firstValue, operator, secondValue);
         display.textContent = calcValue;
@@ -49,78 +50,79 @@ keys.addEventListener('click', function (e) {
         calculator.dataset.firstValue = displayedNum;
       }
 
-      key.classList.add('is-depressed');
-      calculator.dataset.previousKeyType = 'operator';
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = "operator";
       calculator.dataset.operator = action;
     }
 
-    if (action === 'decimal') {
-      if (!displayedNum.includes('.')) {
-        display.textContent = displayedNum + '.';
-      } else if (
-        previousKeyType === 'operator' ||
-        previousKeyType === 'calculate'
-      ) {
-        display.textContent = '0.';
+    if (action === "uno-minus") {
+      if (!displayedNum.includes("-") || !displayedNum === "0") {
+        display.textContent = "-" + displayedNum;
+      } else if (displayedNum.includes("-")) {
+        display.textContent = display.textContent.split("-").join("");
       }
-      calculator.dataset.previousKeyType = 'decimal';
+      calculator.dataset.previousKeyType = "uno-minus";
     }
 
-    if (action !== 'clear') {
-      const clearButton = calculator.querySelector('[data-action=clear]');
-      clearButton.textContent = 'CE';
+    if (action === "decimal") {
+      if (!displayedNum.includes(".")) {
+        display.textContent = displayedNum + ".";
+      } else if (
+        previousKeyType === "operator" ||
+        previousKeyType === "calculate"
+      ) {
+        display.textContent = "0.";
+      }
+      calculator.dataset.previousKeyType = "decimal";
     }
 
-    if (action === 'clear') {
-      if (key.textContent === 'AC') {
-        calculator.dataset.firstValue = '';
-        calculator.dataset.modValue = '';
-        calculator.dataset.operator = '';
-        calculator.dataset.previousKeyType = '';
+    if (action !== "clear") {
+      const clearButton = calculator.querySelector("[data-action=clear]");
+      clearButton.textContent = "CE";
+    }
+
+    if (action === "clear") {
+      if (key.textContent === "AC") {
+        calculator.dataset.firstValue = "";
+        calculator.dataset.modValue = "";
+        calculator.dataset.operator = "";
+        calculator.dataset.previousKeyType = "";
       } else {
-        key.textContent = 'AC';
+        key.textContent = "AC";
       }
       display.textContent = 0;
-      calculator.dataset.previousKeyType = 'clear';
+      calculator.dataset.previousKeyType = "clear";
     }
 
-    if (action === 'calculate') {
+    if (action === "calculate") {
       let firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
       let secondValue = displayedNum;
 
       if (firstValue) {
-        if (previousKeyType === 'calculate') {
+        if (previousKeyType === "calculate") {
           firstValue = displayedNum;
           secondValue = calculator.dataset.modValue;
         }
         display.textContent = calculate(firstValue, operator, secondValue);
-        console.log(display.textContent, firstValue, operator, secondValue)
       }
       calculator.dataset.modValue = secondValue;
-      calculator.dataset.previousKeyType = 'calculate';
+      calculator.dataset.previousKeyType = "calculate";
     }
 
-
-    Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
-
-  };
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove("is-depressed")
+    );
+  }
 });
 
-function calculate(n1, operator, n2) {
-  let result = "";
-  if (operator === 'add') {
-    result = (parseFloat(n1 * 1000) + parseFloat(n2 * 1000)) / 1000;
-  } else if (operator === 'subtract') {
-    result = parseFloat(n1) - parseFloat(n2);
-  } else if (operator === 'multiply') {
-    result = parseFloat(n1) * parseFloat(n2);
-  } else if (operator === 'divide') {
-    result = parseFloat(n1) / parseFloat(n2);
-  } else if (operator === 'power') {
-    result = Math.pow(parseFloat(n1), parseFloat(n2));
-  } else if (operator === 'square-root') {
-    result = Math.sqrt(parseFloat(n1));
-  }
-  return result;
-}
+const calculate = (n1, operator, n2) => {
+  const firstNum = parseFloat(n1);
+  const secondNum = parseFloat(n2);
+  if (operator === "add") return (firstNum * 1000 + secondNum * 1000) / 1000;
+  if (operator === "subtract") return (firstNum * 1000 - secondNum * 1000) / 1000;
+  if (operator === "multiply") return (firstNum * 1000 * (secondNum * 1000)) / 1000000;
+  if (operator === "divide") return firstNum / secondNum;
+  if (operator === "power") return firstNum ** secondNum;
+  if (operator === "square-root") return Math.sqrt(firstNum);
+};
