41abb8614f3b96c75e741011e9569d01cec2cb28
nin3439 <ninok33_94@mail.ru>
2020-10-26 16:24:10 +0300

    feat: finish burger on page pets

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -3,10 +3,31 @@ const navbar = document.querySelector('.navbar');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
 
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('toggle-navbar');
-    menuIcon.classList.toggle('rotate-hamburger-icon');
-    body.classList.toggle('disable-body');
-    overlay.classList.toggle('show-overlay');
+let isMenuOpen = false;
 
+menuIcon.addEventListener('click', () => {
+    if (isMenuOpen) {
+        navbar.classList.toggle('toggle-navbar');
+        menuIcon.classList.add('rotate-hamburger-icon-back');
+        menuIcon.classList.remove('rotate-hamburger-icon');
+        body.classList.toggle('disable-body');
+        overlay.classList.toggle('show-overlay');
+        isMenuOpen = false;
+    } else {
+        navbar.classList.toggle('toggle-navbar');
+        menuIcon.classList.add('rotate-hamburger-icon');
+        menuIcon.classList.remove('rotate-hamburger-icon-back');
+        body.classList.toggle('disable-body');
+        overlay.classList.toggle('show-overlay');
+        isMenuOpen = true;
+    }
 });
+
+let pets = [];
+const request = new XMLHttpRequest();
+request.open('GET', './pets.json');
+request.onload = () => {console.log(request.response)};
+fetch('pets.json').then(res => res.json()).then(list => {
+  pets = list;
+})
+console.log(pets);
